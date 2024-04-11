af58ee5c1db805a8d9f48bf084089322bfce0980
joelsilva333 <joelpitra44@gmail.com>
2024-04-11 01:47:06 +0100

    create: backend com funcionalidades de getAll e createTask

---
+++ backend/src/models/tasksModel.js
@@ -0,0 +1,23 @@
+const connection = require('./connection')
+
+const getAll = async () => {
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
+    return tasks;
+};
+
+const createTask = async (task) => {
+    const {title} = task;
+
+    const dateUTC = new Date(Date.now()).toUTCString();
+
+    const query = 'INSERT INTO task(title, status, created_at) VALUES (?, ?, ?, ?)';
+
+    const [createdTask] = await connection.execute(query, [title, 'pendente', dateUTC]);
+
+    return createdTask;
+};
+
+module.exports = {
+    getAll,
+    createTask,
+};
